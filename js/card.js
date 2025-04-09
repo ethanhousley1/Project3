33e863ca11f6fb6de26fc7d41c2cb56b29b0fed9
ethanhousley1 <ethanhousley@gmail.com>
2025-04-09 10:58:18 -0600

    beginning of randomizer

--- js/card.js
+++ js/card.js
@@ -1,12 +1,14 @@
 // this JS file is for the trading card, which is probably our main product.
 
-let allData; // api data stored globally
+let allData; // api data stored globally, not really used yet
 // it must import the API
+
+function randomInt (min, max) {return Math.floor(Math.random() * (max-min + 1)) + min; }; // creating a random int function for randomizer
 
 fetch('https://sports.is120.ckearl.com')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        console.log(data.data);
         allData = data.data;
         for (let leagueName in allData) {
             console.log(leagueName);
@@ -15,9 +17,34 @@ fetch('https://sports.is120.ckearl.com')
             body = document.querySelector('body');
             body.appendChild(leagueList)
         }
+
+
+        //beginning of randomizer
+    
+        // randomizes the league
+        randomLeagueInt = randomInt(0,3);
+        console.log(randomLeagueInt)
+        // clunky but this converts a random int into the league
+        if (randomLeagueInt === 3) {
+            randomLeague = 'nhl';
+        } else if (randomLeagueInt === 2) {
+            randomLeague = 'mlb';
+        } else if (randomLeagueInt === 1) {
+            randomLeague = 'nba';
+        } else {
+            randomLeague = 'nfl';
+        }
+        console.log(randomLeague);
+
+        console.log(allData[randomLeague]);
+
     })
     .catch(error => {
         console.error('Error:', error)
     });
 
-;
+
+
+
+
+
