d409316a4c2210c4366fa3129b2148e490308299
ethanhousley1 <ethanhousley@gmail.com>
2025-04-15 19:37:09 -0600

    added a global variable specificPlayerArray, which is the array that all the searched players are stored in.

--- js/card.js
+++ js/card.js
@@ -17,11 +17,12 @@ body = document.querySelector('body');
 body.appendChild(playerStats);
 
 let cardWrapperCounter = 0;
+let specificPlayerArray = [];
 
 fetch('https://sports.is120.ckearl.com')
     .then(response => response.json())
     .then(data => {
-        allData = data.data;
+        allData = data;
 
         // Calling the randomplayer function to get a random card populated on the hero page if it is the index.html page
         if (window.location.pathname.includes("index.html")) {
@@ -99,7 +100,7 @@ fetch('https://sports.is120.ckearl.com')
         // grabs random player, ignores nba
         // we will have to change the logic if he doesn't add nba players to rosters
         if (randomTeam.roster[0] !== null ) {
-            randomPlayer = (randomInt(1, randomTeam.roster.length) -1 )
+            let randomPlayer = (randomInt(1, randomTeam.roster.length) -1 )
             randomPlayer = randomTeam.roster[randomPlayer];
             // console.log(randomPlayer);
             
@@ -108,21 +109,49 @@ fetch('https://sports.is120.ckearl.com')
             // this checks if there is already a card in the container and clears it if there is
             const container = document.querySelector("#example-card");
             container.innerHTML = "";
-
+            let randomPlayerObject = {}; // the object HAS TO be in an array by itself
+            let randomPlayerArray = [];
+            
+            // this is what the player object looks like
+                        // {
+                        //     "fullName":
+                        //     "teamName":
+                        //     "position":
+                        //     "headshot":
+                        //     "height":
+                        //     "weight":
+                        //     "age":
+                        //     "experience":
+                        //     "logo":
+                        //     "div":
+                        // }
+            randomPlayerObject = {
+                "fullName": randomPlayer.fullName,
+                "teamName": randomTeam.name,
+                "position": randomPlayer.position,
+                "headshot": randomPlayer.headshot,
+                "height": randomPlayer.height,
+                "weight": randomPlayer.weight,
+                "age": randomPlayer.age,
+                "experience": randomPlayer.experience,
+                "logo": randomTeam.logo,
+                "div": "#example-card"
+            };
+            // the old way we did it
+            // randomPlayer.fullName,
+            //     randomTeam.name,
+            //     randomPlayer.position,
+            //     randomPlayer.headshot,
+            //     randomPlayer.height,
+            //     randomPlayer.weight,
+            //     randomPlayer.age,
+            //     randomPlayer.experience,
+            //     randomTeam.logo,
+            //     "#example-card"
             // Create and insert new card on hero page
-            createPlayerCard(
-                randomPlayer.fullName,
-                randomTeam.name,
-                randomPlayer.position,
-                randomPlayer.headshot,
-                randomPlayer.height,
-                randomPlayer.weight,
-                randomPlayer.age,
-                randomPlayer.experience,
-                randomTeam.logo,
-                "#example-card"
-            );
-              
+
+            randomPlayerArray.push(randomPlayerObject);
+            createPlayerCard(randomPlayerArray);
         }
     }
 
@@ -134,7 +163,6 @@ fetch('https://sports.is120.ckearl.com')
 
 
 function getSpecificPlayer(allData, searchParam) {
-    specificPlayerArray = [];
     selectedLeagues = ['mlb','nfl','nhl'] // i use this to ditch the nba
     // console.log(allData);
     // console.log(allData[league].teams[team].roster[player].fullName
@@ -146,6 +174,7 @@ function getSpecificPlayer(allData, searchParam) {
                     if (specificPlayer.fullName.toLowerCase().includes(searchParam)) {
                         // console.log(specificPlayer.fullName);
                         // displayPlayer(specificPlayer);
+                        
                         createPlayerObject = {
                             "fullName": specificPlayer.fullName,
                             "teamName": allData[league].teams[team].name,
@@ -159,7 +188,6 @@ function getSpecificPlayer(allData, searchParam) {
                             "div" : "#card-grid",
                         }
                         specificPlayerArray.push(createPlayerObject);
-                        console.log(createPlayerObject);
     
                     }
                 }
@@ -175,152 +203,153 @@ function getSpecificPlayer(allData, searchParam) {
 //name, team, position, imageSrc, height, weight, age, experience, logoSrc, containerId
 
 function createPlayerCard(specificPlayerArray) {
-    
-    for (let i = 0; i < 20; i++) {
+    console.log(specificPlayerArray.length);
+    for (let i = 0; i < specificPlayerArray.length && i < 20; i++) {
         let specificPlayer = specificPlayerArray[i];
         let name = specificPlayer.fullName;
         let team = specificPlayer.team;
         let position = specificPlayer.position;
-
+        let imageSrc = specificPlayer.headshot;
+        let height = specificPlayer.height;
+        let weight = specificPlayer.weight;
+        let age = specificPlayer.age;
+        let experience = specificPlayer.experience;
+        let logoSrc = specificPlayer.logo;
+        let containerId = specificPlayer.div;
 
         
-    }
-    console.log(cardWrapperCounter);
-    if (cardWrapperCounter > 20) {
-        return
-    }
-    const parentContainer = document.querySelector(containerId);
+        const parentContainer = document.querySelector(containerId);
+        
+        const cardWrapper = document.createElement("div");
+        cardWrapper.className = "card-wrapper";
     
-    const cardWrapper = document.createElement("div");
-    cardWrapper.className = "card-wrapper";
-  
-    const card = document.createElement("div");
-    card.className = "card";
-  
-    // FRONT
-    const front = document.createElement("div");
-    front.className = "card-face front";
-  
-    const nameContainer = document.createElement("div");
-    nameContainer.id = "player-name-container";
-    nameContainer.style.backgroundColor = '#EEFOEB';
-  
-    const playerName = document.createElement("div");
-    playerName.id = "player-name";
-    playerName.textContent = name;
-  
-    const playerTeam = document.createElement("div");
-    playerTeam.id = "player-team";
-    playerTeam.textContent = team;
-  
-    nameContainer.appendChild(playerName);
-    nameContainer.appendChild(playerTeam);
-  
-    const imageContainer = document.createElement("div");
-    imageContainer.id = "player-image-container";
-  
-    const image = document.createElement("img");
-    image.id = "player-image";
-    image.src = imageSrc;
-  
-    imageContainer.appendChild(image);
-  
-    const positionDiv = document.createElement("div");
-    positionDiv.id = "player-position";
-    positionDiv.style.backgroundColor = '#EEFOEB';
-    positionDiv.textContent = position;
-  
-    front.appendChild(nameContainer);
-    front.appendChild(imageContainer);
-    front.appendChild(positionDiv);
-  
-    // BACK
-    const back = document.createElement("div");
-    back.className = "card-face back";
-  
-    back.style.backgroundImage = `url(${logoSrc})`;
-    back.style.backgroundSize = "contain";
-    back.style.backgroundRepeat = "no-repeat";
-    back.style.backgroundPosition = "center";
-
-    const statHeader = document.createElement("h3");
-    statHeader.textContent = "Player Stats";
-    statHeader.style.backgroundColor = '#ffffff';
-    statHeader.style.borderRadius = '5px';
-    statHeader.style.padding = '2px';
-    back.appendChild(statHeader);
-  
-    // Some players didn't have jersey numbers, so we opted to just go for the raw stats available on the API here
-    const statsObj = {
-      Height: height,
-      Weight: weight,
-      Age: age,
-      Experience: experience,
-    };
-
-    const heightPlayer = document.createElement('p');
-    heightPlayer.textContent = `Height: ${Math.floor((statsObj.Height)/12)}' ${(statsObj.Height % 12)}"`;
-    heightPlayer.id = 'back-inner';
-    back.appendChild(heightPlayer);
-
-    const weightPlayer = document.createElement('p');
-    weightPlayer.textContent = `Weight: ${statsObj.Weight} lbs`
-    weightPlayer.id = 'back-inner';
-    back.appendChild(weightPlayer);
-
-    const agePlayer = document.createElement('p');
-    agePlayer.textContent = `Age: ${statsObj.Age} years`
-    agePlayer.id = 'back-inner';
-    back.appendChild(agePlayer);
-
-    const experiencePlayer = document.createElement('p');
-    experiencePlayer.textContent = `Experience: ${statsObj.Experience} years`
-    experiencePlayer.id = 'back-inner';
-    back.appendChild(experiencePlayer);
-
-    // Combine
-    card.appendChild(front);
-    card.appendChild(back);
-    cardWrapper.appendChild(card);
-    parentContainer.appendChild(cardWrapper);
-
-    //create the load more button;
-    loadBtn = document.createElement('button');
+        const card = document.createElement("div");
+        card.className = "card";
     
-}
+        // FRONT
+        const front = document.createElement("div");
+        front.className = "card-face front";
+    
+        const nameContainer = document.createElement("div");
+        nameContainer.id = "player-name-container";
+        nameContainer.style.backgroundColor = '#EEFOEB';
+    
+        const playerName = document.createElement("div");
+        playerName.id = "player-name";
+        playerName.textContent = name;
+    
+        const playerTeam = document.createElement("div");
+        playerTeam.id = "player-team";
+        playerTeam.textContent = team;
+    
+        nameContainer.appendChild(playerName);
+        nameContainer.appendChild(playerTeam);
+    
+        const imageContainer = document.createElement("div");
+        imageContainer.id = "player-image-container";
+    
+        const image = document.createElement("img");
+        image.id = "player-image";
+        image.src = imageSrc;
+    
+        imageContainer.appendChild(image);
+    
+        const positionDiv = document.createElement("div");
+        positionDiv.id = "player-position";
+        positionDiv.style.backgroundColor = '#EEFOEB';
+        positionDiv.textContent = position;
+    
+        front.appendChild(nameContainer);
+        front.appendChild(imageContainer);
+        front.appendChild(positionDiv);
+    
+        // BACK
+        const back = document.createElement("div");
+        back.className = "card-face back";
+    
+        back.style.backgroundImage = `url(${logoSrc})`;
+        back.style.backgroundSize = "contain";
+        back.style.backgroundRepeat = "no-repeat";
+        back.style.backgroundPosition = "center";
 
-// Making a function that will print the card collection
-// this will be used for the card collection page
-// it will use createPlayerCard
-function populateAllCards(allData, containerId) {
-    const container = document.querySelector(containerId);
-    container.innerHTML = ""; // Clear previous cards if any
-  
-    const selectedLeagues = ["mlb", "nfl", "nhl"];
-  
-    selectedLeagues.forEach((league) => {
-      const leagueData = allData[league];
-      if (!leagueData) return;
-  
-      leagueData.teams.forEach((team) => {
-        team.roster.forEach((player) => {
-          // Only create card if player data is complete
-          if (player && player.fullName && player.headshot) {
-            createPlayerCard(
-              player.fullName,
-              team.name,
-              player.position,
-              player.headshot,
-              player.height,
-              player.weight,
-              player.age,
-              player.experience,
-              team.logo,
-              containerId
-            );
-          }
+        const statHeader = document.createElement("h3");
+        statHeader.textContent = "Player Stats";
+        statHeader.style.backgroundColor = '#ffffff';
+        statHeader.style.borderRadius = '5px';
+        statHeader.style.padding = '2px';
+        back.appendChild(statHeader);
+    
+        // Some players didn't have jersey numbers, so we opted to just go for the raw stats available on the API here
+        const statsObj = {
+        Height: height,
+        Weight: weight,
+        Age: age,
+        Experience: experience,
+        };
+
+        const heightPlayer = document.createElement('p');
+        heightPlayer.textContent = `Height: ${Math.floor((statsObj.Height)/12)}' ${(statsObj.Height % 12)}"`;
+        heightPlayer.id = 'back-inner';
+        back.appendChild(heightPlayer);
+
+        const weightPlayer = document.createElement('p');
+        weightPlayer.textContent = `Weight: ${statsObj.Weight} lbs`
+        weightPlayer.id = 'back-inner';
+        back.appendChild(weightPlayer);
+
+        const agePlayer = document.createElement('p');
+        agePlayer.textContent = `Age: ${statsObj.Age} years`
+        agePlayer.id = 'back-inner';
+        back.appendChild(agePlayer);
+
+        const experiencePlayer = document.createElement('p');
+        experiencePlayer.textContent = `Experience: ${statsObj.Experience} years`
+        experiencePlayer.id = 'back-inner';
+        back.appendChild(experiencePlayer);
+
+        // Combine
+        card.appendChild(front);
+        card.appendChild(back);
+        cardWrapper.appendChild(card);
+        parentContainer.appendChild(cardWrapper);
+
+        //create the load more button;
+        loadBtn = document.createElement('button');
+        
+    }
+
+    // Making a function that will print the card collection
+    // this will be used for the card collection page
+    // it will use createPlayerCard
+    function populateAllCards(allData, containerId) {
+        const container = document.querySelector(containerId);
+        container.innerHTML = ""; // Clear previous cards if any
+    
+        const selectedLeagues = ["mlb", "nfl", "nhl"];
+    
+        selectedLeagues.forEach((league) => {
+        const leagueData = allData[league];
+        if (!leagueData) return;
+    
+        leagueData.teams.forEach((team) => {
+            team.roster.forEach((player) => {
+            // Only create card if player data is complete
+            if (player && player.fullName && player.headshot) {
+                createPlayerCard(
+                player.fullName,
+                team.name,
+                player.position,
+                player.headshot,
+                player.height,
+                player.weight,
+                player.age,
+                player.experience,
+                team.logo,
+                containerId
+                );
+            }
+            });
         });
-      });
-    });
+        });
+    }
 }
-  
